238a4cf1662d8e211103293f9b55ce136d50f318
roshan123dandgavhal <roshandandgavhal@gmail.com>
2020-07-30 16:39:33 +0530

    Bug Solved

--- meeting.php
+++ meeting.php
@@ -90,7 +90,6 @@ class ZoomApi extends DB{
 
 		//echo __FUNCTION__;
 		//echo __CLASS__.'::'.__FUNCTION__; exit;
-		
 		if(isset($someArray['code']) && $someArray['code'] == 124) {
 			self::refreshToken();
 			call_user_func(__CLASS__.'::'.__FUNCTION__);
@@ -190,10 +189,117 @@ class ZoomApi extends DB{
 		
 		exit();
 	}
+
+	/*
+	* Update Meeting
+	*/
+	public static function updateMeeting($array = array(), $loc_meeting_id) {
+		$curl_post_url = "https://api.zoom.us/v2/meetings/$loc_meeting_id";
+
+		if(count($array) == 0) {
+			$array = array(
+		        "topic" => "Anula Function",
+		        "type" => 2,
+		        "start_time" => "2020-08-05T20:30:00",
+		        "duration" => "30", // 30 mins
+		        "password" => "123456",
+		        "timezone" => 'Asia/Kolkata'
+	    	);
+		}
+	    
+	    $postdata = json_encode($array); //exit;
+
+	    $ch = curl_init();
+	    curl_setopt ($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	    curl_setopt ($ch, CURLOPT_MAXREDIRS, 3);
+	    curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, false);
+	    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt ($ch, CURLOPT_VERBOSE, 0);
+	    curl_setopt ($ch, CURLOPT_HEADER, 1);
+	    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	    curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	    curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	    curl_setopt ($ch, CURLOPT_POST, true);
+	    curl_setopt ($ch, CURLOPT_HEADER, false);
+	    curl_setopt ($ch, CURLOPT_URL, $curl_post_url);
+	    curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
+	    curl_setopt ($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer ".self::$loc_token,'Content-Type: application/json'));
+	    curl_setopt ($ch, CURLOPT_POSTFIELDS, $postdata);
+	    curl_setopt ($ch, CURLOPT_TIMEOUT, 0);
+	    $result = curl_exec($ch);
+	    //echo $result; 
+	    curl_close($ch);
+	    $someArray = json_decode($result, true);
+		
+		if(isset($someArray['code']) && $someArray['code'] == 124) {
+			self::refreshToken();
+			call_user_func(__CLASS__.'::'.__FUNCTION__);
+		} else {
+			//echo "<pre>";
+			//print_r($someArray);
+			return $someArray;
+		}
+		
+		//exit();
+	}
+
+	/*
+	* Delete Meeting List
+	*/
+	public static function deleteMeeting($loc_meeting_id) {
+		$userEmail = "suryajhealthcare@gmail.com";
+		$curl_post_url = "https://api.zoom.us/v2/meetings/$loc_meeting_id";
+		$postdata	= array();
+		$ch			= curl_init();
+
+		curl_setopt_array($ch, 
+			array(CURLOPT_URL => $curl_post_url,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING => "",
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 0,
+				CURLOPT_FOLLOWLOCATION => 1,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "DELETE",
+				CURLOPT_SSL_VERIFYHOST=> 0,  
+				CURLOPT_SSL_VERIFYPEER=> 0,
+				CURLOPT_HTTPHEADER => array(
+					"Authorization: Bearer ".self::$loc_token
+				),
+			)
+		);
+		$result	= curl_exec($ch);
+		$err	= curl_error($ch);
+		curl_close($ch);
+
+		$someArray = json_decode($result, true);
+
+		if(isset($someArray['code']) && $someArray['code'] == 124) {
+			self::refreshToken();
+			call_user_func(__CLASS__.'::'.__FUNCTION__);
+		} else {
+			echo "<pre>";
+			print_r($someArray);
+		}
+		
+		exit();
+	}
 }
 
 new ZoomApi();
-if ($_GET['callbackaction'] == "refreshToken") { ZoomApi::refreshToken(); } 
-if ($_GET['callbackaction'] == "getProfile") { ZoomApi::getProfile(); } 
-if ($_GET['callbackaction'] == "createMeeting") { ZoomApi::createMeeting(); } 
-if ($_GET['callbackaction'] == "getMeetingList") { ZoomApi::getMeetingList(); } 
+if (isset($_GET['callbackaction']) && $_GET['callbackaction'] == "refreshToken") { ZoomApi::refreshToken(); } 
+if (isset($_GET['callbackaction']) && $_GET['callbackaction'] == "getProfile") { ZoomApi::getProfile(); } 
+if (isset($_GET['callbackaction']) && $_GET['callbackaction'] == "createMeeting") { ZoomApi::createMeeting(); } 
+if (isset($_GET['callbackaction']) && $_GET['callbackaction'] == "getMeetingList") { ZoomApi::getMeetingList(); }
+
+$updatearray = array(
+		        "topic" => "Wellness Activity UPDATED",
+		        "type" => 2,
+		        "start_time" => "2020-08-05T20:30:00",
+		        "duration" => "30", // 30 mins
+		        "password" => "123456",
+		        "timezone" => 'Asia/Kolkata'
+	    	);
+
+if (isset($_GET['callbackaction']) && $_GET['callbackaction'] == "updateMeeting") { ZoomApi::updateMeeting($updatearray,"86215597499"); } 
+if (isset($_GET['callbackaction']) && $_GET['callbackaction'] == "deleteMeeting") { ZoomApi::deleteMeeting("123456"); } 
