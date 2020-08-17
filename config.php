bf22c03612e60a5b001b1eb9a7408b6fd7dab524
roshan123dandgavhal <roshandandgavhal@gmail.com>
2020-08-17 15:46:36 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+require_once "class-db.php";
+ 
+define('CLIENT_ID', '9q3t7x5fR9WWoA4V2Tau9g');
+define('CLIENT_SECRET', 'aj2ts1p8o5PEf2JvYLa4GTfZPUBh1Uz8');
+define('REDIRECT_URI', 'http://lbmserver:84/roshan_test/zoom/response.php');
+
+/*
+<?php
+require_once "class-db.php";
+ 
+define('CLIENT_ID', '7jPUyzWuRgW2aL4uDC2PtA');
+define('CLIENT_SECRET', 'b0V8JJv84YmBCscNHxMcuhoA8ydNVXe1');
+define('REDIRECT_URI', 'http://lbmserver:84/roshan_test/zoom/response.php');*/
