53f4dd593ac526fdc976a9d33abbb0bef912dad9
roshan123dandgavhal <roshandandgavhal@gmail.com>
2020-08-17 15:48:33 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,4 +3,4 @@ require_once "class-db.php";
  
 define('CLIENT_ID', 'YOUR_CLIENT_ID');
 define('CLIENT_SECRET', 'YOUR_CLIENT_SECRET_KEY');
-define('REDIRECT_URI', 'YOUR_REDIRECT_UTL');
+define('REDIRECT_URI', 'YOUR_REDIRECT_URL');
